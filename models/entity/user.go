e4b42dfc2d4048e336eef505ff6528b89e85bbc9
zenglingzhi <zenglingzhi@jiangroom.com>
2020-01-07 15:09:15 +0800

    新增数据库注释

--- models/entity/user.go
+++ models/entity/user.go
@@ -3,14 +3,14 @@ package entity
 import "time"
 
 type User struct {
-	Id   int `form:"id" orm :"auto,pk"` //默认//Id   int `orm:"pk;auto;column(user_id)"` //表示设置为主键并且自增，列名为user_id
-	RealName string `orm:"size(100)" json:"realName" form:"realName"`
-	UserName string `orm:"size(100)"column(user_name)`
-	Password string `orm:"size(100)"column(password)`
-	Phone string `orm:"size(100)"`
-	Avatar string `orm:"size(200)"`
-	Sex  int `orm:"size(2);column(gender)"`
+	Id         int       `form:"id" orm :"auto,pk"` //默认//Id   int `orm:"pk;auto;column(user_id)"` //表示设置为主键并且自增，列名为user_id
+	RealName   string    `orm:"size(100)" json:"realName" form:"realName" description:"用户真实姓名"`
+	UserName   string    `orm:"size(100)"column(user_name)`
+	Password   string    `orm:"size(100)"column(password)`
+	Phone      string    `orm:"size(100)"`
+	Avatar     string    `orm:"size(200)"`
+	Sex        int       `orm:"size(2);column(gender)"`
 	CreateDate time.Time `orm:"type(date);column(create_date)"`
-	LoginDate time.Time `orm:"type(date);column(login_date)"`
-	Remark string `orm:"size(200)"column(remark)`
+	LoginDate  time.Time `orm:"type(date);column(login_date)"`
+	Remark     string    `orm:"size(200)"column(remark)`
 }
